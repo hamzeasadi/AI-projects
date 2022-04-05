5f0fc955fb4285e11676fa04f3e74f3495e26b46
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-05 08:59:50 +0100

    hyper parameters and layers created

--- src/model.py
+++ src/model.py
@@ -5,10 +5,30 @@ from torch import nn as nn
 
 
 
+# PLS hyper-parameters
+# cnv = [in_channels, out_chnnels, kernel_size, strids]
+pls_conv_params = {'cnv1':[1, 8, 7, 5], 'cnv2':[8, 32, 5, 3], 'cnv3':[32, 64, 4, 2], 'output_dim':1, 'input_dim':600}
+pls_linear_params = {'input_dim': 10, 'output_dim': 1, 'h1':8, 'h2': 5}
+
 class PLS(nn.Module):
 
     def __init__(self, **kwargs):
         super(PLS, self).__init__()
+        self.blk1 = self._block(inCh=kwargs['cnv1'][0], outCh=kwargs['cnv1'][1], krSz=kwargs['cnv1'][2], stride=kwargs['cnv1'][3])
+        self.blk2 = self._block(inCh=kwargs['cnv2'][0], outCh=kwargs['cnv2'][1], krSz=kwargs['cnv2'][2], stride=kwargs['cnv2'][3])
+        self.blk1 = self._block(inCh=kwargs['cnv3'][0], outCh=kwargs['cnv3'][1], krSz=kwargs['cnv3'][2], stride=kwargs['cnv3'][3])
+        self.gap = nn.AvgPool1d(kernel_size=18, stride=1)
+        self.sigmoid = nn.Sigmoid()
+
+        self.gap = nn.AvgPool1d()
+
+
+    def _block(self, inCh, outCh, krSz, stride, pdd=0):
+        return nn.Sequential(
+            nn.Conv1d(in_channels=inCh, out_channels=outCh, kernel_size=krSz, padding=pdd),
+            nn.BatchNorm1d(outCh),
+            nn.LeakyReLU(negative_slope=0.1)
+        )
 
 
 
