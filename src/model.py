520d2a08462d2d3580a415bbcac3a27d0884b31d
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-06 09:03:37 +0100

    encoder model implemented

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+from operator import neg
 import torch
 import numpy as np
 import pandas as pd
@@ -13,6 +14,27 @@ encoder_linear_params = {
 
 class Encoder(nn.Module):
     
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__()
+        self.blk1 = self._block(inFeat=kwargs['input_dim'], outFeat=kwargs['h1'])
+        self.blk2 = self._block(inFeat=kwargs['h1'], outFeat=kwargs['h2'])
+        self.mu = nn.Linear(in_features=kwargs['h2'], out_features=kwargs['latent_dim'])
+        self.logvar = nn.Linear(in_features=kwargs['h2'], out_features=kwargs['latent_dim'])
+
+
+    def _block(self, inFeat, outFeat):
+        return nn.Sequential(
+            nn.Linear(in_features=inFeat, out_features=outFeat),
+            nn.BatchNorm1d(1), nn.LeakyReLU(negative_slope=0.1)
+        )
+
+    def forward(self, x):
+        x = self.blk1(x)
+        x = self.blk2(x)
+        mu = self.mu(x)
+        logvar = self.logvar(x)
+
+        return mu, logvar
 
 
 
