c52bf7ee7e6445c8856320e30390eac274215751
hamzeasadi <hamzeasadi1@gmail.com>
2022-04-06 08:55:49 +0100

    encoder hypere patameters linear

--- src/model.py
+++ src/model.py
@@ -5,6 +5,14 @@ from torch import nn as nn
 
 
 
+# model hyper
+encoder_linear_params = {
+    'h1': 500, 'h2': 100, 'input_dim':2000, 'latent_dim': 10
+}
+
+
+class Encoder(nn.Module):
+    
 
 
 
